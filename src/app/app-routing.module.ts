1baa092ef4c8bc8d37f87f2669da432176be6734
Shyam Kumar K G <shyam719@gmail.com>
2020-09-19 20:12:57 +0530

    Initial Project Setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './layout/admin/admin.component';
+import { AuthComponent } from './layout/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import(
+            './pages/dashboard/dashboard-default/dashboard-default.module'
+          ).then((m) => m.DashboardDefaultModule),
+      },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('./pages/user/profile/profile.module').then(
+            (m) => m.ProfileModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'authentication',
+        loadChildren: () =>
+          import('./pages/auth/auth.module').then((m) => m.AuthModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/shared/menu-items/menu-items.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+
+export interface BadgeItem {
+  type: string;
+  value: string;
+}
+
+export interface ChildrenItems {
+  state: string;
+  target?: boolean;
+  name: string;
+  type?: string;
+  children?: ChildrenItems[];
+}
+
+export interface MainMenuItems {
+  state: string;
+  short_label?: string;
+  main_state?: string;
+  target?: boolean;
+  name: string;
+  type: string;
+  icon: string;
+  badge?: BadgeItem[];
+  children?: ChildrenItems[];
+}
+
+export interface Menu {
+  label: string;
+  main: MainMenuItems[];
+}
+
+const MENUITEMS = [
+  {
+    label: 'Home',
+    main: [
+      {
+        state: 'dashboard',
+        short_label: 'D',
+        name: 'Dashboard',
+        type: 'link',
+        icon: 'ti-home',
+      },
+    ],
+  },
+  {
+    label: 'Master',
+    main: [
+      {
+        state: 'category',
+        short_label: 'C',
+        name: 'Category Master',
+        type: 'link',
+        icon: 'ti-receipt',
+      },
+    ],
+  },
+  {
+    label: 'User',
+    main: [
+      {
+        state: 'authentication',
+        short_label: 'A',
+        name: 'Authentication',
+        type: 'sub',
+        icon: 'ti-id-badge',
+        children: [
+          {
+            state: 'login',
+            type: 'link',
+            name: 'Login',
+            target: true,
+          },
+          {
+            state: 'registration',
+            type: 'link',
+            name: 'Registration',
+            target: true,
+          },
+        ],
+      },
+      {
+        state: 'user',
+        short_label: 'U',
+        name: 'User Profile',
+        type: 'link',
+        icon: 'ti-user',
+      },
+    ],
+  },
+];
+
+@Injectable()
+export class MenuItems {
+  getAll(): Menu[] {
+    return MENUITEMS;
+  }
+
+  /*add(menu: Menu) {
+    MENUITEMS.push(menu);
+  }*/
+}
